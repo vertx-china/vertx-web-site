7b7a3fdd252a80cd7e89791c8e55945874b01537
gcnyin <guchounongyin@gmail.com>
2020-12-30 20:34:06 +0800

    vertx-web开头的几行

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({ title }) => (
     <Head>
       <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-      <meta name="description" content="Vert.x | Reactive applications on the JVM"/>
+      <meta name="description" content="Vert.x | JVM上的响应式应用"/>
       <meta name="robots" content="index,follow"/>
       <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet"/>
       <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400" rel="stylesheet"/>
